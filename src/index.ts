ea9a6d4ae2654668689ecef7ade616e21642b9c4
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-14 20:51:30 +0100

    starter

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/**
+ *  l'objectif est de faire la logique suivante 
+ */
+
+/**
+ * 
+ *   instancier number of collection 
+ *   instance string collection 
+ *   creating the holder of a sorter
+ *   abstract the the sorting algorithm 
+ *   
+ *   le programme en JS sera plus facile à lire mais non pas facile à maintenir si on a un large scale d'application 
+ * 
+ *   const numbers = new Class();
+ *   const string = new Class();
+ * 
+ *    instancier le sorter 
+ *    puis effectuer le sorting 
+ * 
+ *   
+ */
+
