a6ff2ba0e9ca2f437adfae5e1fce696a9540830a
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-14 22:52:14 +0100

    type guards

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
-/**
- *  l'objectif est de faire la logique suivante 
- */
+import { Sorter } from "./classes/Sorter";
+const sorter = new Sorter([-2,-3,10,4,8]);
+sorter.sort();
+console.log(sorter.collection);
 
-/**
- * 
- *   instancier number of collection 
- *   instance string collection 
- *   creating the holder of a sorter
- *   abstract the the sorting algorithm 
- *   
- *   le programme en JS sera plus facile à lire mais non pas facile à maintenir si on a un large scale d'application 
- * 
- *   const numbers = new Class();
- *   const string = new Class();
- * 
- *    instancier le sorter 
- *    puis effectuer le sorting 
- * 
- *   
- */
-
-
-console.log('hello world');
-console.log('test')
