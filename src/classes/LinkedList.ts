fc84f94c5e42d1ae9e85bb3f9799a0bb111a848a
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-15 19:42:46 +0100

    implementing the algorithm installing pretty console

--- src/classes/LinkedList.ts
+++ src/classes/LinkedList.ts
@@ -1,87 +1,90 @@
-class NodeElement {
-  next: NodeElement | null = null;
-  constructor(public data: number) {}
-}
-
-export class LinkedList {
-  head: NodeElement | null = null;
-
-  add(data: number): void {
-    const node = new NodeElement(data);
-    if (!this.head) {
-      this.head = node;
-      return;
-    }
-
-    let tail = this.head;
-    while (tail.next) {
-      tail = tail.next;
-    }
+class Node {
+    next: Node | null = null;
+  
+    constructor(public data: number) {}
   }
-
-  get length(): number {
-    if (!this.head) {
-      return 0;
-    }
-
-    let length = 1;
-    let node = this.head;
-
-    while (node.next) {
-      length++;
-      node = node.next;
-    }
-
-    return length;
-  }
-
-  atPosition(index: number): NodeElement {
-    if (!this.head) {
-      throw new Error("index is out of scoope");
-    }
-
-    let counter = 0;
-    let node = this.head as NodeElement;
-
-    while (node) {
-      if (counter === index) {
-        return node;
-      }
-
-      counter++;
-      node = node.next as NodeElement;
-    }
-
-    throw new Error("Index is out of the scoope");
-  }
-
-  compare(left: number, right: number): boolean {
-    if (!this.head) {
-      throw new Error("List is empty");
-    }
-    return this.atPosition(left).data > this.atPosition(right).data;
-  }
-
-  swap(left: number, right: number): void {
-    let leftTemp = this.atPosition(left);
-    let rightTemp = this.atPosition(right);
-    let temp = leftTemp.data;
-    leftTemp.data = rightTemp.data;
-    rightTemp.data = temp;
-  }
-
-  print():void{
-
-     if(!this.head){
-        console.log('No list')
+  
+  export class LinkedList {
+    head: Node | null = null;
+  
+    add(data: number): void {
+      const node = new Node(data);
+  
+      if (!this.head) {
+        this.head = node;
         return;
-     }
-
-     let node = this.head as NodeElement | null;
-     while(node){
-
+      }
+  
+      let tail = this.head;
+      while (tail.next) {
+        tail = tail.next;
+      }
+  
+      tail.next = node;
+    }
+  
+    get length(): number {
+      if (!this.head) {
+        return 0;
+      }
+  
+      let length = 1;
+      let node = this.head;
+      while (node.next) {
+        length++;
+        node = node.next;
+      }
+  
+      return length;
+    }
+  
+    at(index: number): Node {
+      if (!this.head) {
+        throw new Error('Index out of bounds');
+      }
+  
+      let counter = 0;
+      let node: Node | null = this.head;
+      while (node) {
+        if (counter === index) {
+          return node;
+        }
+  
+        counter++;
+        node = node.next;
+      }
+  
+      throw new Error('Index out of bounds');
+    }
+  
+    compare(leftIndex: number, rightIndex: number): boolean {
+      if (!this.head) {
+        throw new Error('List is empty');
+      }
+  
+      return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+  
+    swap(leftIndex: number, rightIndex: number): void {
+      const leftNode = this.at(leftIndex);
+      const rightNode = this.at(rightIndex);
+  
+      const leftHand = leftNode.data;
+      leftNode.data = rightNode.data;
+      rightNode.data = leftHand;
+    }
+  
+    print(): void {
+      if (!this.head) {
+        return;
+      }
+  
+      let node: Node | null = this.head;
+      while (node) {
         console.log(node.data);
         node = node.next;
-     }
+      }
+    }
   }
-}
+  
+  
