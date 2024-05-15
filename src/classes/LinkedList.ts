e00783ecab15527dc9c93bf10e512cf464905639
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-15 19:18:45 +0100

    implement the linked list methods

--- src/classes/LinkedList.ts
+++ src/classes/LinkedList.ts
@@ -34,4 +34,54 @@ export class LinkedList {
 
     return length;
   }
+
+  atPosition(index: number): NodeElement {
+    if (!this.head) {
+      throw new Error("index is out of scoope");
+    }
+
+    let counter = 0;
+    let node = this.head as NodeElement;
+
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next as NodeElement;
+    }
+
+    throw new Error("Index is out of the scoope");
+  }
+
+  compare(left: number, right: number): boolean {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+    return this.atPosition(left).data > this.atPosition(right).data;
+  }
+
+  swap(left: number, right: number): void {
+    let leftTemp = this.atPosition(left);
+    let rightTemp = this.atPosition(right);
+    let temp = leftTemp.data;
+    leftTemp.data = rightTemp.data;
+    rightTemp.data = temp;
+  }
+
+  print():void{
+
+     if(!this.head){
+        console.log('No list')
+        return;
+     }
+
+     let node = this.head as NodeElement | null;
+     while(node){
+
+        console.log(node.data);
+        node = node.next;
+     }
+  }
 }
